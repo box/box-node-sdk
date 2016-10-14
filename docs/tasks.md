0eb5f9e08dd879b78cf849f7e77b8494117e1df5
Peter Toth <peter.toth@hotovo.org>
2016-10-14 12:23:30 +0200

    Create a task
    Implementation, documentation and unit test

---
+++ docs/tasks.md
@@ -0,0 +1,20 @@
+Tasks
+=====
+
+Tasks enabled file-centric workflows in Box. User can create tasks on files and assign them to collaborators on Box.
+
+* [Create a Task](#create-a-task)
+
+Create a Task
+-------------
+
+To create a task call the [`tasks.create(fileID, message, dueAt, callback)`](http://opensource.box.com/box-node-sdk/Tasks.html#create) method.
+
+```js
+client.tasks.create(
+	'1234',
+	'Optional Message',
+	'2014-04-03T11:09:43-07:00',
+	callback
+);
+```
---
+++ tests/lib/managers/tasks-test.js
@@ -0,0 +1,118 @@
+/**
+ * @fileoverview Tasks Manager Tests
+ * @author ptoth
+ */
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+var sinon = require('sinon'),
+	mockery = require('mockery'),
+	leche = require('leche'),
+	BoxClient = require('../../../lib/box-client');
+
+// ------------------------------------------------------------------------------
+// Helpers
+// ------------------------------------------------------------------------------
+var sandbox = sinon.sandbox.create(),
+	boxClientFake = leche.fake(BoxClient.prototype),
+	Tasks,
+	tasks,
+	BASE_PATH = '/tasks',
+	FILE_ID = '1234',
+	REVIEW_ACTION = 'review',
+	MODULE_FILE_PATH = '../../../lib/managers/tasks';
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+describe('Tasks', function() {
+
+	before(function() {
+		// Enable Mockery
+		mockery.enable({
+			useCleanCache: true
+		});
+		// Register Mocks
+		mockery.registerAllowable('../util/url-path');
+		mockery.registerAllowable(MODULE_FILE_PATH);
+	});
+
+	beforeEach(function() {
+		// Setup File Under Test
+		Tasks = require(MODULE_FILE_PATH);
+		tasks = new Tasks(boxClientFake);
+	});
+
+	afterEach(function() {
+		sandbox.verifyAndRestore();
+		mockery.resetCache();
+	});
+
+	after(function() {
+		mockery.deregisterAll();
+		mockery.disable();
+	});
+
+	describe('create()', function() {
+
+		var message,
+			dueAt,
+			expectedParams;
+
+		beforeEach(function() {
+			message = 'Optional message for the test';
+			dueAt = '2014-04-03T11:09:43-07:00';
+			expectedParams = {
+				body: {
+					item: {
+						type: 'file',
+						id: FILE_ID
+					},
+					action: REVIEW_ACTION
+				}
+			};
+		});
+
+		it('should make POST request with all parameters to create a task when all optional parameters are passed', function() {
+			expectedParams.body.message = message;
+			expectedParams.body.due_at = dueAt;
+
+			sandbox.stub(boxClientFake, 'defaultResponseHandler');
+			sandbox.mock(boxClientFake).expects('post').withArgs(BASE_PATH, expectedParams);
+			tasks.create(FILE_ID, message, dueAt);
+		});
+
+		it('should make POST request with message to create a task when when just a message is passed', function() {
+			expectedParams.body.message = message;
+
+			sandbox.stub(boxClientFake, 'defaultResponseHandler');
+			sandbox.mock(boxClientFake).expects('post').withArgs(BASE_PATH, expectedParams);
+			tasks.create(FILE_ID, message);
+		});
+
+		it('should make POST request with due_at to create a task when when just a dueAt is passed', function() {
+			expectedParams.body.due_at = dueAt;
+
+			sandbox.stub(boxClientFake, 'defaultResponseHandler');
+			sandbox.mock(boxClientFake).expects('post').withArgs(BASE_PATH, expectedParams);
+			tasks.create(FILE_ID, null, dueAt);
+		});
+
+		it('should make POST request with mandatory parameters to create a task when neither optional parameter is passed', function() {
+			sandbox.stub(boxClientFake, 'defaultResponseHandler');
+			sandbox.mock(boxClientFake).expects('post').withArgs(BASE_PATH, expectedParams);
+			tasks.create(FILE_ID);
+		});
+
+		it('should call BoxClient defaultResponseHandler method with the callback when response is returned', function(done) {
+			sandbox.mock(boxClientFake).expects('defaultResponseHandler').withArgs(done).returns(done);
+			sandbox.stub(boxClientFake, 'post').withArgs(BASE_PATH).yieldsAsync();
+			tasks.create(FILE_ID, message, dueAt, done);
+		});
+
+	});
+
+});
