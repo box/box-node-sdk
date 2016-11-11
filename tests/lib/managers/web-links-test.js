1e32cad2e802e87c13e6c98b25fa1e2696841b9c
Trudko <email@petertoth.sk>
2016-11-11 02:56:06 +0100

    Update Web Link (#56)

--- tests/lib/managers/web-links-test.js
+++ tests/lib/managers/web-links-test.js
@@ -132,6 +132,58 @@ describe('WebLinks', function() {
 			sandbox.stub(boxClientFake, 'get').withArgs(BASE_PATH + '/' + WEB_LINK_ID).yieldsAsync();
 			weblinks.get(WEB_LINK_ID, null, done);
 		});
+	});
+
+	describe('update()', function() {
+		var name,
+			description,
+			expectedParams;
+
+		beforeEach(function() {
+			name = 'Box Website!';
+			description = 'Cloud Content Management';
+			expectedParams = {
+				body: {
+				}
+			};
+		});
+
+		it('should make PUT request with all parameters to update a weblink when all optional parameters are passed', function() {
+			expectedParams.body.name = name;
+			expectedParams.body.description = description;
+
+			sandbox.stub(boxClientFake, 'defaultResponseHandler');
+			sandbox.mock(boxClientFake).expects('put').withArgs(BASE_PATH + '/' + WEB_LINK_ID, expectedParams);
+			weblinks.update(WEB_LINK_ID, {name: name, description: description});
+		});
+
+		it('should make PUT request with name to update a weblink when just a name is passed', function() {
+			expectedParams.body.name = name;
+
+			sandbox.stub(boxClientFake, 'defaultResponseHandler');
+			sandbox.mock(boxClientFake).expects('put').withArgs(BASE_PATH + '/' + WEB_LINK_ID, expectedParams);
+			weblinks.update(WEB_LINK_ID, {name: name});
+		});
+
+		it('should make PUT request with description to update a weblink when just a description is passed', function() {
+			expectedParams.body.description = description;
+
+			sandbox.stub(boxClientFake, 'defaultResponseHandler');
+			sandbox.mock(boxClientFake).expects('put').withArgs(BASE_PATH + '/' + WEB_LINK_ID, expectedParams);
+			weblinks.update(WEB_LINK_ID, {description: description});
+		});
+
+		it('should make PUT request with mandatory parameters to create a task when neither optional parameter is passed', function() {
+			sandbox.stub(boxClientFake, 'defaultResponseHandler');
+			sandbox.mock(boxClientFake).expects('put').withArgs(BASE_PATH + '/' + WEB_LINK_ID, expectedParams);
+			weblinks.update(WEB_LINK_ID, null);
+		});
+
+		it('should call BoxClient defaultResponseHandler method with the callback when response is returned', function(done) {
+			sandbox.mock(boxClientFake).expects('defaultResponseHandler').withArgs(done).returns(done);
+			sandbox.stub(boxClientFake, 'put').withArgs(BASE_PATH + '/' + WEB_LINK_ID).yieldsAsync();
+			weblinks.update(WEB_LINK_ID, {name: name, description: description}, done);
+		});
 
 	});
 
