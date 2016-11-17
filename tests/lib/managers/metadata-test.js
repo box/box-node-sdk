bd3151ac530cf44b1f2e968fd4b5ed560332402a
Matt Willer <mwiller@box.com>
2016-11-17 00:54:17 -0800

    Add metadata template Create and Update

--- tests/lib/managers/metadata-test.js
+++ tests/lib/managers/metadata-test.js
@@ -74,4 +74,73 @@ describe('Metadata', function() {
 			metadata.getTemplates('enterprise', done);
 		});
 	});
+
+	describe('createTemplate()', function() {
+
+		it('should make a POST request to create metadata template when called', function(done) {
+
+			var name = 'Vendor Contract',
+				fields = [
+					{
+						type: 'string',
+						key: 'category',
+						displayName: 'Category'
+					},
+					{
+						type: 'enum',
+						key: 'fy',
+						displayName: 'FY',
+						options: [
+							{key: 'FY11'},
+							{key: 'FY12'},
+							{key: 'FY13'},
+							{key: 'FY14'},
+							{key: 'FY15'}
+						]
+					}
+				],
+				options = {
+					hidden: true
+				};
+
+			var expectedParams = {
+				body: {
+					scope: 'enterprise',
+					displayName: name,
+					fields: fields,
+					hidden: true
+				}
+			};
+
+			sandbox.mock(boxClientFake).expects('post').withArgs('/metadata_templates/schema', expectedParams).yieldsAsync();
+			sandbox.stub(boxClientFake, 'defaultResponseHandler').returnsArg(0);
+			metadata.createTemplate(name, fields, options, done);
+		});
+	});
+
+	describe('updateTemplate()', function() {
+
+		it('should make PUT call to update template when called', function(done) {
+
+			var scope = 'enterprise',
+				template = 'vendorContract',
+				operations = [
+					{
+						op: 'editField',
+						fieldKey: 'category',
+						data: {
+							displayName: 'Contract Category'
+						}
+					}
+				];
+
+			var expectedParams = {
+				body: operations
+			};
+
+			sandbox.mock(boxClientFake).expects('put').withArgs('/metadata_templates/' + scope + '/' + template + '/schema', expectedParams).yieldsAsync();
+			sandbox.stub(boxClientFake, 'defaultResponseHandler').returnsArg(0);
+			metadata.updateTemplate(scope, template, operations, done);
+		});
+	});
 });
