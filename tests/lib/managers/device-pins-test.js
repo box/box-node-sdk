d5c13dd85fe6ffcbb70f9ce877d4bb4b20c23321
Matt Willer <mwiller@box.com>
2016-11-16 19:07:38 -0800

    Get Enterprise Device Pins
    Implemented getting a list of all enterprise device pins with
    automatic population of the current enterprise ID, since users can
    only be in one enterprise.

--- tests/lib/managers/device-pins-test.js
+++ tests/lib/managers/device-pins-test.js
@@ -7,21 +7,24 @@
 // ------------------------------------------------------------------------------
 // Requirements
 // ------------------------------------------------------------------------------
-var sinon = require('sinon'),
+var assert = require('chai').assert,
+	sinon = require('sinon'),
 	mockery = require('mockery'),
 	leche = require('leche');
 
-var BoxClient = require('../../../lib/box-client');
-
+var BoxClient = require('../../../lib/box-client'),
+	Users = require('../../../lib/managers/users');
 
 // ------------------------------------------------------------------------------
 // Helpers
 // ------------------------------------------------------------------------------
 var sandbox = sinon.sandbox.create(),
 	boxClientFake,
+	usersFake,
 	DevicePins,
 	devicePins,
 	PIN_ID = '873645',
+	CURRENT_USER_ID = 'me',
 	MODULE_FILE_PATH = '../../../lib/managers/device-pins';
 
 
@@ -34,6 +37,9 @@ describe('DevicePins', function() {
 	beforeEach(function() {
 		// Setup Environment
 		boxClientFake = leche.fake(BoxClient.prototype);
+		usersFake = leche.fake(Users.prototype);
+		boxClientFake.users = usersFake;
+		boxClientFake.CURRENT_USER_ID = CURRENT_USER_ID;
 		// Register Mocks
 		mockery.enable({ useCleanCache: true });
 		mockery.registerAllowable('../util/url-path');
@@ -111,6 +117,64 @@ describe('DevicePins', function() {
 			sandbox.stub(boxClientFake, 'del').withArgs('/device_pinners/' + PIN_ID, {qs: null}).yieldsAsync();
 			devicePins.delete(PIN_ID, null, done);
 		});
+	});
+
+	describe('getAll()', function() {
+
+		var ENTERPRISE_ID = '7898345';
+
+		it('should get current enterprise ID and make GET request to get device pin info when user is in enterprise', function(done) {
+
+			var params = {
+				qs: null
+			};
+
+			var user = {
+				enterprise: {
+					id: ENTERPRISE_ID
+				}
+			};
+
+			sandbox.stub(boxClientFake, 'defaultResponseHandler').returnsArg(0);
+			sandbox.mock(boxClientFake.users).expects('get').withArgs('me', {fields: 'enterprise'}).yieldsAsync(null, user);
+			sandbox.mock(boxClientFake).expects('get').withArgs('/enterprises/' + ENTERPRISE_ID + '/device_pinners', params).yieldsAsync();
+			devicePins.getAll(null, done);
+		});
+
+		it('should get current enterprise ID and call callback with error when user is not in enterprise', function(done) {
+
+			var user = {
+				enterprise: null
+			};
+
+			sandbox.stub(boxClientFake, 'defaultResponseHandler').returnsArg(0);
+			sandbox.mock(boxClientFake.users).expects('get').withArgs('me', {fields: 'enterprise'}).yieldsAsync(null, user);
+			sandbox.mock(boxClientFake).expects('get').never();
+			devicePins.getAll(null, function(err) {
+
+				assert.instanceOf(err, Error);
+				done();
+			});
+		});
+
+		it('should call BoxClient defaultResponseHandler method with the callback when response is returned', function(done) {
+
+			var user = {
+				enterprise: {
+					id: ENTERPRISE_ID
+				}
+			};
+
+			var pins = {
+				entries: []
+			};
+
+			sandbox.mock(boxClientFake).expects('defaultResponseHandler').withArgs(done).returns(done);
+			sandbox.stub(boxClientFake.users, 'get').yieldsAsync(null, user);
+			sandbox.stub(boxClientFake, 'get').yieldsAsync(null, pins);
+			devicePins.getAll(null, done);
+		});
+
 	});
 
 });
