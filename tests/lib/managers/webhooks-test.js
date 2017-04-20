1a4705d2e3a0edfdf62691d6b009a7a6d0dc5fa1
Karthik Shanmugasundaram <kshanmugasundaram@box.com>
2017-04-19 20:21:41 -0700

    Webhooks primary secondary key code review changes

--- tests/lib/managers/webhooks-test.js
+++ tests/lib/managers/webhooks-test.js
@@ -153,6 +153,24 @@ describe('Webhooks', function() {
 
 	});
 
+	describe('setPrimarySignatureKey()', function() {
+		const PRIMARY_SIGNATURE_KEY = 'SamplePrimaryKey';
+		it('should validate the primary signature key is set', function() {
+			Webhooks.setPrimarySignatureKey(PRIMARY_SIGNATURE_KEY);
+
+			assert.equal(PRIMARY_SIGNATURE_KEY, Webhooks.primarySignatureKey);
+		});
+	});
+
+	describe('setSecondarySignatureKey()', function() {
+		const SECONDARY_SIGNATURE_KEY = 'SampleSecondaryKey';
+		it('should validate the secondary signature key is set', function() {
+			Webhooks.setSecondarySignatureKey(SECONDARY_SIGNATURE_KEY);
+
+			assert.equal(SECONDARY_SIGNATURE_KEY, Webhooks.secondarySignatureKey);
+		});
+	});
+
 	describe('validateMessage()', function() {
 
 		// A sample webhook message that is signed with 'SamplePrimaryKey' and 'SampleSecondaryKey':
