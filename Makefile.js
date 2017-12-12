50a9398200f320c7df4855c33cd45605765f95e3
Matt Willer <mwiller@box.com>
2017-12-11 19:15:42 -0800

    Add nsp to test for security vulnerabilities

--- Makefile.js
+++ Makefile.js
@@ -85,6 +85,9 @@ target.lint = function() {
 
 target.test = function() {
 	var code = target.lint();
+	if (semver.gte(process.version.substr(1), '6.0.0')) {
+		code += nodeCLI.exec('nsp', 'check').code;
+	}
 	code += nodeCLI.exec('nyc', MOCHA_BINARY, '-c', '-R spec', '--exit', TEST_FILES).code;
 
 	if (code) {
