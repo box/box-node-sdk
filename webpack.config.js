45862f0025d79742b099191240c9b1a94d903df6
amgrobelny-box <agrobelny@box.com>
2018-01-24 16:06:09 -0800

    Basics for webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const path = require('path');
+const env = require('yargs').argv.mode;
+
+const libraryName = 'BoxSdk';
+
+let plugins = [], outputFile;
+
+outputFile = libraryName + '.min.js';
+plugins.push(new UglifyJsPlugin({
+  sourceMap: true,
+  uglifyOptions: {
+    compress: { warnings: false },
+    ecma: 5
+  }
+}));
+
+let config = {
+  entry: __dirname + '/lib/box-node-sdk.js',
+  devtool: '#source-map',
+  output: {
+    path: __dirname + '/client_build',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    sourceMapFilename: libraryName + '.map'
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "eslint-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;
