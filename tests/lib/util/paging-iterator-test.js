d0e5672c3e0e2936e9bcf88e8a692ee03887c6dd
Matt Willer <mwiller@box.com>
2019-02-12 12:24:36 -0800

    Return events collection as JSON when iterators are enabled (#341)

--- tests/lib/util/paging-iterator-test.js
+++ tests/lib/util/paging-iterator-test.js
@@ -51,6 +51,28 @@ describe('PagingIterator', function() {
 		mockery.disable();
 	});
 
+	describe('isIterable', function() {
+
+		it('should return false when the collection is from the event stream', function() {
+			var response = {
+				body: {
+					next_stream_position: '123456',
+					chunk_size: 1,
+					entries: [
+						{
+							type: 'event',
+							event_id: '98765'
+						}
+					]
+				},
+				request: {
+					method: 'GET'
+				}
+			};
+
+			assert.isFalse(PagingIterator.isIterable(response));
+		});
+	});
 
 	describe('constructor', function() {
 
