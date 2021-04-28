a4d55528012032cade51ab77b782d57cb19b00ee
Marcin Hagmajer <mhagmajer@users.noreply.github.com>
2021-04-28 19:31:49 +0200

    ts migrate: src/util/config.ts (#591)

--- src/util/config.ts
+++ src/util/config.ts
@@ -2,23 +2,22 @@
  * @fileoverview Configuration Object
  */
 
-'use strict';
+import assert = require('assert');
+import * as https from 'https';
+import * as url from 'url';
+import ProxyAgent = require('proxy-agent');
 
 // ------------------------------------------------------------------------------
 // Requirements
 // ------------------------------------------------------------------------------
-var assert = require('assert'),
-	https = require('https'),
-	merge = require('merge-options'),
-	sdkVersion = require('../../package.json').version,
-	ProxyAgent = require('proxy-agent'),
-	url = require('url');
+const merge = require('merge-options'),
+	sdkVersion = require('../../package.json').version;
 
 // ------------------------------------------------------------------------------
 // Private
 // ------------------------------------------------------------------------------
 
-var nodeVersion = process.version;
+const nodeVersion = process.version;
 
 /**
  * Configuration for App Auth
@@ -30,6 +29,14 @@ var nodeVersion = process.version;
  * @property {int} [expirationTime=30] Number of seconds the JWT should live for
  * @property {boolean} [verifyTimestamp=false] Whether the timestamp when the auth token is created should be validated
  */
+type AppAuthConfig = {
+	keyID: string;
+	privateKey: string | Buffer;
+	passphrase: string;
+	algorithm: 'RS256' | 'RS384' | 'RS512';
+	expirationTime: number;
+	verifyTimestamp: boolean;
+};
 
 /**
  * Configuration settings used to initialize and customize the SDK
@@ -48,6 +55,28 @@ var nodeVersion = process.version;
  * @property {boolean} [request.strictSSL] Set to false to disable strict SSL checking, which allows using Dev APIs [Default: true]
  * @property {?AppAuthConfig} appAuth Optional configuration for App Auth
  */
+type UserConfigurationOptions = {
+	clientID: string;
+	clientSecret: string;
+	apiRootURL: string;
+	uploadAPIRootURL: string;
+	authorizeRootURL: string;
+	uploadRequestTimeoutMS: number;
+	retryIntervalMS: number;
+	numMaxRetries: number;
+	expiredBufferMS: number;
+	request: {
+		agentClass: any /* FIXME */;
+		agentOptions: any /* FIXME */;
+		strictSSL: boolean;
+	};
+	appAuth?: AppAuthConfig;
+	proxy?: {
+		url: string;
+		username: string;
+		password: string;
+	};
+};
 
 var defaults = {
 	clientID: null,
@@ -77,7 +106,7 @@ var defaults = {
 		// Use an agent with keep-alive enabled to avoid performing SSL handshake per connection
 		agentClass: https.Agent,
 		agentOptions: {
-			keepAlive: true
+			keepAlive: true,
 		},
 		// Encode requests as JSON. Encode the response as well if JSON is returned.
 		json: true,
@@ -87,15 +116,15 @@ var defaults = {
 		followRedirect: false,
 		// By default, we attach a version-specific user-agent string to SDK requests
 		headers: {
-			'User-Agent': `Box Node.js SDK v${sdkVersion} (Node ${nodeVersion})`
-		}
-	}
+			'User-Agent': `Box Node.js SDK v${sdkVersion} (Node ${nodeVersion})`,
+		},
+	},
 };
 
 var appAuthDefaults = {
 	algorithm: 'RS256',
 	expirationTime: 30,
-	verifyTimestamp: false
+	verifyTimestamp: false,
 };
 
 /**
@@ -105,10 +134,16 @@ var appAuthDefaults = {
  * @throws {AssertionError}
  * @private
  */
-function validateBasicParams(params) {
+function validateBasicParams(params: UserConfigurationOptions) {
 	// Assert that the given params valid, and that required values are present
-	assert(typeof params.clientID === 'string', '"clientID" must be set via init() before using the SDK.');
-	assert(typeof params.clientSecret === 'string', '"clientSecret" must be set via init() before using the SDK.');
+	assert(
+		typeof params.clientID === 'string',
+		'"clientID" must be set via init() before using the SDK.'
+	);
+	assert(
+		typeof params.clientSecret === 'string',
+		'"clientSecret" must be set via init() before using the SDK.'
+	);
 }
 
 /**
@@ -118,22 +153,38 @@ function validateBasicParams(params) {
  * @throws {AssertionError}
  * @private
  */
-function validateAppAuthParams(appAuth) {
-	assert(typeof appAuth.keyID === 'string', 'Key ID must be provided in app auth params');
-	assert(typeof appAuth.privateKey === 'string' || appAuth.privateKey instanceof Buffer, 'Private key must be provided in app auth params');
-	assert(typeof appAuth.passphrase === 'string' && appAuth.passphrase.length > 0, 'Passphrase must be provided in app auth params');
+function validateAppAuthParams(appAuth: AppAuthConfig) {
+	assert(
+		typeof appAuth.keyID === 'string',
+		'Key ID must be provided in app auth params'
+	);
+	assert(
+		typeof appAuth.privateKey === 'string' ||
+			appAuth.privateKey instanceof Buffer,
+		'Private key must be provided in app auth params'
+	);
+	assert(
+		typeof appAuth.passphrase === 'string' && appAuth.passphrase.length > 0,
+		'Passphrase must be provided in app auth params'
+	);
 
-	var validAlgorithms = [
-		'RS256',
-		'RS384',
-		'RS512'
-	];
+	var validAlgorithms = ['RS256', 'RS384', 'RS512'];
 	if (typeof appAuth.algorithm !== 'undefined') {
-		assert(validAlgorithms.indexOf(appAuth.algorithm) > -1, `Algorithm in app auth params must be one of: ${validAlgorithms.join(', ')}`);
+		assert(
+			validAlgorithms.indexOf(appAuth.algorithm) > -1,
+			`Algorithm in app auth params must be one of: ${validAlgorithms.join(
+				', '
+			)}`
+		);
 	}
 
 	if (typeof appAuth.expirationTime !== 'undefined') {
-		assert(Number.isInteger(appAuth.expirationTime) && appAuth.expirationTime > 0 && appAuth.expirationTime <= 60, 'Valid token expiration time (0 - 60) must be provided in app auth params');
+		assert(
+			Number.isInteger(appAuth.expirationTime) &&
+				appAuth.expirationTime > 0 &&
+				appAuth.expirationTime <= 60,
+			'Valid token expiration time (0 - 60) must be provided in app auth params'
+		);
 	}
 }
 
@@ -143,12 +194,21 @@ function validateAppAuthParams(appAuth) {
  * @returns {void}
  * @private
  */
-function updateRequestAgent(params) {
+function updateRequestAgent(
+	params: UserConfigurationOptions &
+		Required<Pick<UserConfigurationOptions, 'proxy'>>
+) {
 	if (params.proxy.url) {
 		params.request.agentClass = ProxyAgent;
-		params.request.agentOptions = Object.assign({}, params.request.agentOptions, url.parse(params.proxy.url));
+		params.request.agentOptions = Object.assign(
+			{},
+			params.request.agentOptions,
+			url.parse(params.proxy.url)
+		);
 		if (params.proxy.username && params.proxy.password) {
-			Object.assign(params.request.agentOptions, {auth: `${params.proxy.username}:${params.proxy.password}`});
+			Object.assign(params.request.agentOptions, {
+				auth: `${params.proxy.username}:${params.proxy.password}`,
+			});
 		}
 	}
 }
@@ -166,40 +226,49 @@ function updateRequestAgent(params) {
  * @param {UserConfigurationOptions} params - The config options set by the user
  * @constructor
  */
-function Config(params) {
+class Config {
+	_params: Required<UserConfigurationOptions>;
 
-	validateBasicParams(params);
-	if (typeof params.appAuth === 'object') {
-		validateAppAuthParams(params.appAuth);
-		params.appAuth = merge({}, appAuthDefaults, params.appAuth);
+	constructor(params: UserConfigurationOptions) {
+		validateBasicParams(params);
+		if (typeof params.appAuth === 'object') {
+			validateAppAuthParams(params.appAuth);
+			params.appAuth = merge({}, appAuthDefaults, params.appAuth);
+		}
+
+		// Ensure that we don't accidentally assign over Config methods
+		assert(
+			!params.hasOwnProperty('extend'),
+			'Config params may not override Config methods'
+		);
+		assert(
+			!params.hasOwnProperty('_params'),
+			'Config params may not override Config methods'
+		);
+
+		// Set the given params or default value if params property is missing
+		this._params = merge(defaults, params);
+		updateRequestAgent(this._params);
+		Object.assign(this, this._params);
+		// Freeze the object so that configuration options cannot be modified
+		Object.freeze(this);
 	}
 
-	// Ensure that we don't accidentally assign over Config methods
-	assert(!params.hasOwnProperty('extend'), 'Config params may not override Config methods');
-	assert(!params.hasOwnProperty('_params'), 'Config params may not override Config methods');
-
-	// Set the given params or default value if params property is missing
-	this._params = merge(defaults, params);
-	updateRequestAgent(this._params);
-	Object.assign(this, this._params);
-	// Freeze the object so that configuration options cannot be modified
-	Object.freeze(this);
+	/**
+	 * Extend the current config into a new config with new params overriding old ones
+	 * @param {UserConfigurationOptions} params The override options
+	 * @returns {Config} The extended configuration
+	 */
+	extend(params: UserConfigurationOptions) {
+		var newParams = merge({}, this._params, params);
+		delete newParams.extend;
+		delete newParams._params;
+		return new Config(newParams);
+	}
 }
-
-/**
- * Extend the current config into a new config with new params overriding old ones
- * @param {UserConfigurationOptions} params The override options
- * @returns {Config} The extended configuration
- */
-Config.prototype.extend = function(params) {
-	var newParams = merge({}, this._params, params);
-	delete newParams.extend;
-	delete newParams._params;
-	return new Config(newParams);
-};
 
 /**
  * @module box-node-sdk/lib/util/config
  * @see {@Link Config}
  */
-module.exports = Config;
+export = Config;
