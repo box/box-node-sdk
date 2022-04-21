3554d41d9050e7a81224c35e3e2e257604a0b41b
Kamil Berdychowski <kberdychowski@box.com>
2022-04-21 15:02:10 +0200

    fix: added missing `js-docs` on shared links (#731)

--- src/managers/folders.ts
+++ src/managers/folders.ts
@@ -16,15 +16,48 @@ import errors from '../util/errors';
 type FolderSharedLinkAccess = 'open' | 'company' | 'collaborators' | null;
 
 type FolderSharedLinkPermissions = {
-	can_view?: boolean,
+	/**
+	 * If the shared link allows only to view folders. This can only be set when access is set to open or company.
+	 */
+	can_view?: true,
+	/**
+	 * If the shared link allows only to download folders. This can only be set when access is set to open or company.
+	 */
 	can_download?: boolean
 }
 
 type FolderSharedLink = {
+	/**
+	 * The level of access for the shared link. This can be restricted to anyone with the link (open),
+	 * only people within the company (company) and only those who have been invited to the file (collaborators).
+	 *
+	 * If not set, this field defaults to the access level specified by the enterprise admin.
+	 * To create a shared link with this default setting pass the shared_link object with no access field.
+	 * To remove access and change its value to default one pass the shared_link object with null value access field.
+	 */
 	access?: FolderSharedLinkAccess,
+	/**
+	 * The password required to access the shared link. Set the password to null to remove it.
+	 * A password can only be set when access is set to open.
+	 */
 	password?: string | null,
+	/**
+	 * The timestamp at which this shared link will expire. This field can only be set by users with paid accounts.
+	 * The value must be greater than the current date and time.
+	 * Example value: '2012-12-12T10:53:43-08:00'
+	 */
 	unshared_at?: string | null,
+	/**
+	 * Defines a custom vanity name to use in the shared link URL, for example vanity_name: "my-shared-link" will
+	 * produce a shared link of "https://app.box.com/v/my-shared-link".
+	 *
+	 * Custom URLs should not be used when sharing sensitive content as vanity URLs are a lot easier to guess
+	 * than regular shared links.
+	 */
 	vanity_name?: string | null,
+	/**
+	 * Defines what actions are allowed on a shared link.
+	 */
 	permissions?: FolderSharedLinkPermissions
 }
 // ------------------------------------------------------------------------------
