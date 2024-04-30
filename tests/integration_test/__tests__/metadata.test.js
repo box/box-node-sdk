4517893664a70d90111818f54649519544d8ff46
Artur Jankowski <ajankowski@box.com>
2024-04-30 14:31:47 +0200

    fix tests

--- tests/integration_test/__tests__/metadata.test.js
+++ tests/integration_test/__tests__/metadata.test.js
@@ -1,76 +1,71 @@
 'use strict';
 
-const {getAppClient} = require('../context');
-const {createBoxTestFolder} = require('../objects/box-test-folder');
-const {createBoxTestFile} = require('../objects/box-test-file');
+const { getAppClient } = require('../context');
+const { createBoxTestFolder } = require('../objects/box-test-folder');
+const { createBoxTestFile } = require('../objects/box-test-file');
 const utils = require('../lib/utils');
 const path = require('path');
 const context = {};
 
-beforeAll(async() => {
+beforeAll(async () => {
 	context.appClient = await getAppClient();
 	let folder = await createBoxTestFolder(context.appClient);
 	context.folder = folder;
 });
 
-afterAll(async() => {
+afterAll(async () => {
 	await context.folder.dispose();
 	context.folder = null;
 });
 
-test('test matadata search', async() => {
+test('test metadata search', async () => {
 	const templateKey = `template_${utils.randomName()}`;
 	const fields = [
 		{
 			type: 'float',
 			key: 'testFloatValue',
-			displayName: 'testFloatValue',
-		},
+			displayName: 'testFloatValue'
+		}
 	];
 
 	const metadataTemplate = await context.appClient.metadata.createTemplate(templateKey, fields, {
 		scope: 'enterprise',
-		templateKey: templateKey,
+		templateKey: templateKey
 	});
 
-	expect(metadataTemplate.id)
-			.toBeDefined();
-	expect(metadataTemplate.templateKey)
-			.toBe(templateKey);
-	expect(metadataTemplate.displayName)
-			.toBe(templateKey);
-
-	const file = await createBoxTestFile(context.appClient, path.join(__dirname, '../resources/blank.pdf'), 'blank_sign_1.pdf', context.folder.id);
+	expect(metadataTemplate.id).toBeDefined();
+	expect(metadataTemplate.templateKey).toBe(templateKey);
+	expect(metadataTemplate.displayName).toBe(templateKey);
 	try {
-		const metadata = await context.appClient.files.addMetadata(file.id, 'enterprise', templateKey, {testFloatValue: 150});
-		expect(metadata.$template)
-				.toBe(templateKey);
-		expect(metadata.testFloatValue)
-				.toBe(150);
+		const file = await createBoxTestFile(context.appClient, path.join(__dirname, '../resources/blank.pdf'), 'blank_sign_1.pdf', context.folder.id);
+		try {
+			const metadata = await context.appClient.files.addMetadata(file.id, 'enterprise', templateKey, { testFloatValue: 150 });
+			expect(metadata.$template).toBe(templateKey);
+			expect(metadata.testFloatValue).toBe(150);
 
-		const searchForm = `${metadataTemplate.scope}.${metadataTemplate.templateKey}`;
-		const ancestorFolderId = '0';
-		const queryOptions = {
-			query: 'testFloatValue >= :arg',
-			query_params: {arg: '100'},
-			limit: 1,
-			order_by: [
-				{
-					'field_key': 'testFloatValue',
-					'direction': 'asc',
-				},
-			],
-		};
-		const searchResults = await context.appClient.metadata.query(searchForm, ancestorFolderId, queryOptions);
-		expect(searchResults.entries.length)
-				.toBe(1);
-		expect(searchResults.entries[0].name)
-				.toBe(file.name);
+			const searchForm = `${metadataTemplate.scope}.${metadataTemplate.templateKey}`;
+			const ancestorFolderId = '0';
+			const queryOptions = {
+				query: 'testFloatValue >= :arg',
+				query_params: { arg: '100' },
+				limit: 1,
+				order_by: [
+					{
+						'field_key': 'testFloatValue',
+						'direction': 'asc'
+					}
+				]
+			};
+			const searchResults = await context.appClient.metadata.query(searchForm, ancestorFolderId, queryOptions);
+			expect(searchResults.entries.length).toBe(1);
+			expect(searchResults.entries[0].name).toBe(file.name);
 
+
+		} finally {
+			await file.dispose();
+		}
+	} finally {
 		await context.appClient.metadata.deleteTemplate('enterprise', metadataTemplate.templateKey);
-	}
-	finally {
-		await file.dispose();
 	}
 }, 120000);
 
