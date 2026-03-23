8a950369c9613d733f18112719f3579b33c0c69f
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2026-03-23 09:50:55 +0100

    docs: Fix links to the Supported AI models page (box/box-openapi#589) (#1376)
    Co-authored-by: box-sdk-build <box-sdk-build@box.com>

--- src/schemas/aiAgentSpreadsheetTool.ts
+++ src/schemas/aiAgentSpreadsheetTool.ts
@@ -23,7 +23,7 @@ import { sdIsList } from '../serialization/json';
 import { sdIsMap } from '../serialization/json';
 export interface AiAgentSpreadsheetTool {
   /**
-   * The model used for the AI agent for spreadsheets. For specific model values, see the [available models list](https://developer.box.com/guides/box-ai/supported-models). */
+   * The model used for the AI agent for spreadsheets. For specific model values, see the [available models list](https://developer.box.com/guides/box-ai/ai-models). */
   readonly model?: string;
   /**
    * The number of tokens for completion. */
